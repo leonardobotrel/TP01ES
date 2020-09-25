a08237e0977aeb4bbdd2832e28b2f6784cb33b98
leonardobotrel <leonardobotrel@gmail.com>
2020-09-24 21:36:53 -0300

    feat #001 - Inclusao de campos
    Foram incluídos os restantes dos campos em cadastro

--- Imobiliaria/resources/views/cadastro.blade.php
+++ Imobiliaria/resources/views/cadastro.blade.php
@@ -31,23 +31,137 @@
             <div class="row">
                 <form action="/action_page.php" class="needs-validation" novalidate>
                     <div class="form-group">
-                        <label for="uname">Número de quartos</label>
+                        <label for="quartos">Número de quartos</label>
                         <input type="number" class="form-control" id="quartos" placeholder="Entre com o n° de quartos" name="nquartos" required>
                         <div class="valid-feedback">Válido</div>
                         <div class="invalid-feedback">Por favor preencha este campo</div>
                     </div>
                     <div class="form-group">
-                        <label for="pwd">Número de suítes</label>
+                        <label for="suites">Número de suítes</label>
                         <input type="number" class="form-control" id="suites" placeholder="Entre com o n° de suites" name="nsuites" required>
                         <div class="valid-feedback">Válido</div>
                         <div class="invalid-feedback">Por favor preencha este campo</div>
                     </div>
+                    <div class="form-group">
+                        <label for="sala_estar">Número de salas de estar</label>
+                        <input type="number" class="form-control" id="sala_estar" placeholder="Entre com o n° de salas de estar" name="nsala_estar" required>
+                        <div class="valid-feedback">Válido</div>
+                        <div class="invalid-feedback">Por favor preencha este campo</div>
+                    </div>
+                    <div class="form-group">
+                        <label for="vagas_garagem">Número de vagas na garagem</label>
+                        <input type="number" class="form-control" id="vagas_garagem" placeholder="Entre com o n° de vagas na garagem" name="nvagas_garagem" required>
+                        <div class="valid-feedback">Válido</div>
+                        <div class="invalid-feedback">Por favor preencha este campo</div>
+                    </div>
+                    <div class="form-group">
+                        <label for="area">Área em metros quadrados</label>
+                        <input type="number" class="form-control" id="area" placeholder="Entre com a área" name="narea" required>
+                        <div class="valid-feedback">Válido</div>
+                        <div class="invalid-feedback">Por favor preencha este campo</div>
+                    </div>
                     <div class="form-group form-check">
-                        <label class="form-check-label">
-                            <input class="form-check-input" type="checkbox" name="remember" required> I agree on blabla.
-                            <div class="valid-feedback">Valid.</div>
-                            <div class="invalid-feedback">Check this checkbox to continue.</div>
-                        </label>
+                        <div>
+                            <label for="armario">Possui armario embutido</label>
+                        </div>
+                        <div class=" form-check form-check-inline">
+                            <input type="radio" class="form-check-input" name="varmario" value="0" required>
+                            <label class="form-check-label">Sim</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input type="radio" class="form-check-input" name="varmario" value="1" require>
+                            <label class="form-check-label">Não</label>
+                            <div class="valid-feedback" style="margin-left: 1.25em">Válido</div>
+                            <div class="invalid-feedback" style="margin-left: 1.25em">Por favor preencha este campo</div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="form-group">
+                            <label for="sala_jantar">Número de salas de jantar</label>
+                            <input type="number" class="form-control" id="sala_jantar" placeholder="Entre com o n° de salas de jantar" name="nsala_jantar" required>
+                            <div class="valid-feedback">Válido</div>
+                            <div class="invalid-feedback">Por favor preencha este campo</div>
+                        </div>
+                        <div class="form-group">
+                            <label for="andar">Número do andar</label>
+                            <input type="number" class="form-control" id="andar" placeholder="Entre com o andar" name="nandar" required>
+                            <div class="valid-feedback">Válido</div>
+                            <div class="invalid-feedback">Por favor preencha este campo</div>
+                        </div>
+                        <div class="form-group">
+                            <label for="valor_condominio">Valor do condominio</label>
+                            <input type="number" min="0.00" step="0.01" class="form-control" id="valor_condominio" placeholder="Entre com o valor do condominio" name="nvalor_condominio" required>
+                            <div class="valid-feedback">Válido</div>
+                            <div class="invalid-feedback">Por favor preencha este campo</div>
+                        </div>
+                        <div class="form-group form-check">
+                            <div>
+                                <label for="armario">Portaria 24 horas</label>
+                            </div>
+                            <div class=" form-check form-check-inline">
+                                <input type="radio" class="form-check-input" name="pportaria" value="0" required>
+                                <label class="form-check-label">Sim</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input type="radio" class="form-check-input" name="pportaria" value="1" require>
+                                <label class="form-check-label">Não</label>
+                                <div class="valid-feedback" style="margin-left: 1.25em">Válido</div>
+                                <div class="invalid-feedback" style="margin-left: 1.25em">Por favor preencha este campo</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="comment">Descrição adcional (opcional)</label>
+                        <textarea class="form-control" rows="5" id="comment"></textarea>
+                    </div>
+                    <div>
+                        <h2>Endereço</h2>
+                    </div>
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <label for="rua">Rua</label>
+                                <input type="text" class="form-control" id="rua" placeholder="Entre com o nome da rua" name="nrua" required>
+                                <div class="valid-feedback">Válido</div>
+                                <div class="invalid-feedback">Por favor preencha este campo</div>
+                            </div>
+                            <div class="col-sm-6">
+                                <label for="numero">Número</label>
+                                <input type="number" class="form-control" id="numero" placeholder="Entre com o número" name="nnumero" required>
+                                <div class="valid-feedback">Válido</div>
+                                <div class="invalid-feedback">Por favor preencha este campo</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="quartos">Bairro</label>
+                        <input type="number" class="form-control" id="quartos" placeholder="Entre com o n° de quartos" name="nquartos" required>
+                        <div class="valid-feedback">Válido</div>
+                        <div class="invalid-feedback">Por favor preencha este campo</div>
+                    </div>
+                    <div class="form-group">
+                        <label for="cidade">Cidade</label>
+                        <input type="text" class="form-control" id="cidade" placeholder="Entre com a cidade" name="ncidade" required>
+                        <div class="valid-feedback">Válido</div>
+                        <div class="invalid-feedback">Por favor preencha este campo</div>
+                    </div>
+                    <div class="form-group">
+                        <label for="cep">CEP (somente números)</label>
+                        <input type="number" class="form-control" id="cep" placeholder="Entre com o cep" name="ncep" required>
+                        <div class="valid-feedback">Válido</div>
+                        <div class="invalid-feedback">Por favor preencha este campo</div>
+                    </div>
+                    <div class="form-group">
+                        <label for="estado">Estado</label>
+                        <input type="text" class="form-control" id="estado" placeholder="Entre com o estado" name="nestado" required>
+                        <div class="valid-feedback">Válido</div>
+                        <div class="invalid-feedback">Por favor preencha este campo</div>
+                    </div>
+                    <div class="form-group">
+                        <label for="pais">País</label>
+                        <input type="text" class="form-control" id="pais" placeholder="Entre com o país" name="npais" required>
+                        <div class="valid-feedback">Válido</div>
+                        <div class="invalid-feedback">Por favor preencha este campo</div>
                     </div>
                     <button type="submit" class="btn btn-primary">Cadastrar</button>
                 </form>
